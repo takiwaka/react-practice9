471b111d77984de453051f6bf6fed2edc5d30f29
kawakita <hakusou.kokuyou@gmail.com>
2021-12-17 06:57:59 +0900

    ルーティングページ設定、header途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <div className="App">
-        <Button colorScheme="teal">ボタン</Button>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
