651840e573fa19c1c1dfb5f34782e6620e155919
kawakita <hakusou.kokuyou@gmail.com>
2021-12-20 07:16:10 +0900

    ログイン機能実装中

--- src/components/atoms/PrimaryButton.tsx
+++ src/components/atoms/PrimaryButton.tsx
@@ -3,15 +3,21 @@ import { memo, ReactNode, VFC } from "react";
 import { Button } from "@chakra-ui/react";
 
 type Props = {
-  children:ReactNode;
+  children: ReactNode;
+  onClick: () => void;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children,onClick } = props;
 
   return (
-    <Button bg="orange.300" color="black" _hover={{ opacity: 0.8 }}>
-     {children}
+    <Button
+      bg="orange.300"
+      color="black"
+      _hover={{ opacity: 0.8 }}
+      onClick={onClick}
+    >
+      {children}
     </Button>
   );
 });
