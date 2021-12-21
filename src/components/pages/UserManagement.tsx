c9d87025e1c9abcc4f41b6b5cc9c5f6aceb1996c
kawakita <hakusou.kokuyou@gmail.com>
2021-12-22 06:00:17 +0900

    カード中央寄せ

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -15,9 +15,9 @@ export const UserManagement: VFC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap p={{ base: 4, md: 10 }}>
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
           {users.map((user) => (
-            <WrapItem mx="auto" key={user.id}>
+            <WrapItem  key={user.id}>
               <UserCard
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
